3dffac0385e61784823f8fc29045dad0f9ed7b79
karinchic301 <kar_rash@mail.ru>
2025-06-19 10:57:18 +0300

    Initial commit: Added calculator module

---
+++ src/calculator.py
@@ -0,0 +1,25 @@
+def add(a: float, b: float) -> float:
+    """
+    Складывает два числа с плавающей точкой.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Сумма двух чисел.
+    """
+    return a + b
+
+def multiply(a: float, b: float) -> float:
+    """
+    Перемножает два числа с плавающей точкой.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Произведение двух чисел.
+    """
+    return a * b
