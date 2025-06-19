3726cb7f26f245b096cb780061f7d875d8db1f97
karinchic301 <kar_rash@mail.ru>
2025-06-19 15:12:11 +0300

    Initial project with calculator and Sphinx docs

---
+++ build/html/_sources/modules.rst.txt
@@ -0,0 +1,8 @@
+Modules
+=======
+
+.. automodule:: calculator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
---
+++ source/conf.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+src_path = os.path.abspath('../src')
+print(f"Добавляем в sys.path: {src_path}")
+
+sys.path.insert(0, src_path)
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'My Project Docs'
+copyright = '2025, Karina'
+author = 'Karina'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+extensions = ['sphinx.ext.autodoc']
+
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
